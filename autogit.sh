d501f8396b5e888d9149c39fbb8bc448854ab920
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-03-16 23:47:40 -0300

    livro_aed_descomp -> Topico: Alocação Estática => add | Prox. Topico: Alocação Dinâmica

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "binaryTree_p1.c : linha 143 e 144 -> add | Prox. Passo : add funcao delete() -> fazer por conta propria"
+git commit -m "livro_aed_descomp -> Topico: Alocação Estática => add | Prox. Topico: Alocação Dinâmica"
 git push
 git status
---
+++ livro_aed_descomp/intro.c
@@ -0,0 +1,19 @@
+/*
+
+1.2 Estrutura de Dados:
+
+É uma forma de armazenar e organizar os dados de modo que possam ser usados de forma eficiente.
+
+1.3 Alocação de Memória:
+
+Temos 2 tipos de alocação de memória:
+
+  1. Alocação Estática => O programador não precisa se preocupar com a reserva de memória para os dados.
+
+  Pois a quant. de memória necessária é armazenada automaticamente pela STACK(pilha) ou em outras partições do programa.
+
+  STACK -> guarda dados alocados dentro dos escopos de funções: variáveis locais, parâmetros, retorno de funções, endereços de outras partes, etc.
+
+  Aqui, os dados são organizados sequencialmente na memória do PC (como arrays). Conhecemos a quantidade de memória utilizada, e não podemos altera-la. Além de ter a possibilidade de armazenar memória que não será utilizada na execução do programa.
+
+*/
