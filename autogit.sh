fc5b75b6d637b11c6223934b965e057b55585890
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-03-29 09:25:58 -0300

    livro_ED_usando_C: cap01.c -> add

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "livro_ED_usando_C folder -> add"
+git commit -m "livro_ED_usando_C: cap01.c -> add"
 git push
 git status
---
+++ livro_ED_com_C/cap01.c
@@ -0,0 +1,15 @@
+/*
+1. Tipos de Dados em C
+*/
+
+/*
+2. Ponteiros em C
+*/
+
+/*
+3. Estruturas de dados em C
+*/
+
+/*
+4. Estruturas em C
+*/
